3ad5641d41f0fd132875e61ab161bd85bc3f47ef
Frank Xu <frank.xu@northbright.com>
2020-05-16 17:19:20 +0800

    Add more comments

--- main.go
+++ main.go
@@ -20,22 +20,28 @@ const (
 	maxMessageType
 )
 
+// Message represents the task message.
 type Message struct {
 	Type   MessageType
 	TaskID int
-	Data   interface{}
+	// Data stores the message data.
+	// For STOPPED, RESUMED and PROGRESSUPDATED, its the percentage(int).
+	Data interface{}
 }
 
 func newMessage(t MessageType, taskID int, data interface{}) *Message {
 	return &Message{t, taskID, data}
 }
 
+// TaskManager is used to start new tasks.
 type TaskManager struct {
 	concurrency int
 	ch          chan *Message
 	sem         chan struct{}
 }
 
+// NewTaskManager returns a new task manager and a channel to consume the task messages.
+// concurrency specify the number of concurrency(goroutines) to do the tasks.
 func NewTaskManager(concurrency int) (*TaskManager, <-chan *Message) {
 	ch := make(chan *Message)
 
@@ -45,6 +51,12 @@ func NewTaskManager(concurrency int) (*TaskManager, <-chan *Message) {
 	return &TaskManager{concurrency, ch, sem}, ch
 }
 
+// Start runs a new task.
+// It can be stopped by calling context.CancelFunc associated with the context.
+// If prevProgress is 0, it's a new task,
+// otherwise, it resumes the task using the previous progress.
+// In real world, "prevProgress" should be replaced with the state of the task.
+// We can use encoding.BinaryMarshaler / encoding.BinaryUnmarshaler to save / load the task state.
 func (m *TaskManager) Start(ctx context.Context, ID int, prevProgress int) {
 	go func() {
 		m.start(ctx, ID, prevProgress)
@@ -102,6 +114,7 @@ BEFORE_START:
 }
 
 func main() {
+	// Create a new task manager which number of concurrency is 2.
 	man, ch := NewTaskManager(2)
 
 	// Use a timeout to exit the program.
@@ -110,14 +123,16 @@ func main() {
 	// Record the cancel func for each task ID.
 	cancelMap := map[int]context.CancelFunc{}
 
+	// Start 4 tasks concurrently.
+	// There should be 2 scheduled tasks.
 	for i := 0; i < 4; i++ {
 		ctx, cancel := context.WithCancel(context.Background())
 		cancelMap[i] = cancel
-		// Start tasks.
 		man.Start(ctx, i, 0)
 	}
 
 	// Stop 1 task randomly.
+	// The stopped task will be resume with previous progress in message loop.
 	go func() {
 		time.Sleep(50 * time.Millisecond)
 		ID := rand.Intn(4)
@@ -125,6 +140,7 @@ func main() {
 		cancel()
 	}()
 
+	// Message loop.
 	for {
 		select {
 		case <-tm:
